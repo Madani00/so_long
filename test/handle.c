382a4e8642c4cedc4f7117815e573c25e0b639c3
El Madani Amchart <123333191+Madani00@users.noreply.github.com>
2025-07-02 11:07:02 +0100

    Delete test directory

--- test/handle.c
+++
@@ -1,5 +0,0 @@
-printf("Game structure initialized: %p\n", game);
-	printf("MLX initialized: %p\n", game->mlx);
-	printf("Map rows: %d, columns: %d\n", game->row, game->column);
-	if (!game->map)
-		return (1); // Error handling
